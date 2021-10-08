0d541edc469a739253fe636cad1d5b65c29c8589
jvycee <jvycee@protonmail.com>
2021-10-07 23:47:16 -0600

    Added code and variables

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+var angle = PI / 4;
+
+var slider;
+
 function setup() {
   createCanvas(400, 400);
+  slider = createSlider(0, TWO_PI, PI / 4);
 }
 
 function draw() {
-  background(220);
+  background(51);
+  angle = slider.value();
+  stroke(255);
+  translate(200, height);
+  branch(100);
+}
+
+function branch(len) {
+
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  rotate(angle);
+  if (len > 4) {
+    branch(len * 0.67);
+  }
+
 }
