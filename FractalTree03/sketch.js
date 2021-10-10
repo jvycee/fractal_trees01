c3bacc081309f4a1b56effac81191eb3420b6265
jvycee <jvycee@protonmail.com>
2021-10-10 12:59:05 -0600

    v1 final version

--- FractalTree03/sketch.js
+++ FractalTree03/sketch.js
@@ -1,7 +1,74 @@
+// variables: A B
+// axiom: A
+// rules: (A => AB), (B => A)
+
+let axiom = "F";
+let sentence = axiom;
+let rules = [];
+let len = 100;
+let angle;
+
+rules[0] = {
+  a: "F",
+  b: "FF+[+F-F-F]-[-F+F+F]"
+};
+
+
+function generate() {
+  len *= 0.5;
+  let nextSentence = "";
+  for (let i = 0; i < sentence.length; i++) {
+    let current = sentence.charAt(i);
+    let found = false;
+    for (let j = 0; j < rules.length; j++) {
+      if (current == rules[j].a) {
+        found = true;
+        nextSentence += rules[j].b;
+        break;
+      }
+    }
+    if (!found) {
+      nextSentence += current;
+    }
+  }
+  sentence = nextSentence;
+  createP(sentence);
+  turtle();
+}
+
+function turtle() {
+  background(51);
+  resetMatrix();
+  translate(width / 2, height);
+  stroke(171, 32, 253, 100);
+  for (let i = 0; i < sentence.length; i++) {
+    let current = sentence.charAt(i);
+
+    if (current == "F") {
+      line(0, 0, 0, -len);
+      translate(0, -len);
+    } else if (current == "+") {
+      rotate(angle);
+    } else if (current == "-") {
+      rotate(-angle);
+    } else if (current == "[") {
+      push();
+    } else if (current == "]") {
+      pop();
+    }
+  }
+}
+
 function setup() {
   createCanvas(400, 400);
+  angle = radians(25);
+  background(51);
+  createP(axiom);
+  turtle();
+  let button = createButton("generate");
+  button.mousePressed(generate);
 }
 
 function draw() {
-  background(220);
+  // background(220);
 }
